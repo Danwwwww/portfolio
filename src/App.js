b1f65faeaee0c3e827fd4e32373845c4d61c3db6
Danwwww <f44t44@gmail.com>
2024-09-01 23:33:18 +0800

    tgBot

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import css from "./images/CSS.svg.png";
 import js from "./images/JS.png";
 import github from "./images/Github.png";
 import { useRef, useState, useEffect } from "react";
-import hamburgerIcon from "./images/hamburgerIcon.png";
-import arrow from "./images/arrow.png";
 import infroIcon from "./images/infro.png";
 import projectIcon from "./images/project.png";
 import contactIcon from "./images/contact.png";
@@ -15,6 +13,7 @@ import homeIcon from "./images/home.png";
 import me from "./images/me.png";
 import burgerGif from "./images/BurgerPic.gif";
 import chromeGif from "./images/Chrome.gif";
+import tgBotGif from "./images/tgbot.gif";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -182,7 +181,16 @@ function App() {
   }
 
   function Home() {
-    const [scale, setScale] = useState(Array(4).fill(false));
+    let skills = [
+      { name: "HTML", color: "#e44d26" },
+      { name: "CSS", color: "#254de5" },
+      { name: "Javascript", color: "#f7d723" },
+      { name: "Typescript", color: "#3074c0" },
+      { name: "React", color: "#25d1f4" },
+      { name: "NodeJS", color: "#7cb700" },
+      { name: "NestJS", color: "#e32743" },
+      { name: "MongoDB", color: "#00e661" },
+    ];
 
     return (
       <div className="intro" ref={homeRef}>
@@ -207,30 +215,21 @@ function App() {
         </div>
         <div className="bottomIntro">
           <p className="skill">Skill sets :</p>
-          <div className="skillImgs">
-            {/* <img
-              className="skillImg"
-              src="https://www.svgrepo.com/show/374118/tailwind.svg"
-            /> */}
-            <img
-              className="skillImg"
-              src="https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/16073828/742161_74060.png"
-            />
-            <img
-              className="skillImg"
-              src="https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/16073828/227525_754519.png"
-            />
-            <img
-              className="skillImg"
-              src="https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/16073828/166463_947098.png"
-            />
-            <img className="skillImg" src={js} />
-            <img className="skillImg" src={html} />
-            <img className="skillImg" src={css} />
-            <img
-              className="skillImg"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1024px-Typescript_logo_2020.svg.png"
-            />
+          <div className="skillContainer">
+            {" "}
+            {skills.map((e, i) => {
+              return (
+                <div key={i} className="tagContainer" data-aos="fade-left">
+                  <span
+                    className="beforeTag"
+                    style={{ backgroundColor: e.color }}
+                  ></span>
+                  <div className="tag" style={{ backgroundColor: e.color }}>
+                    {e.name}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
@@ -244,24 +243,11 @@ function App() {
         <div className="about" data-aos="zoom-in">
           <p className="title aboutTitle">About Me</p>
           <p className="aboutIntro">
-            {"\u00A0".repeat(4)}I recently graduated from a Front-end boot-camp
-            by{" "}
-            <span>
-              <a
-                target="blank"
-                href="https://en.hongkong.generation.org/front-end-web-developer/"
-              >
-                Clap@JC X Generation
-              </a>
-            </span>{" "}
-            .I learnt the various practical coding skills. Additionally, I
-            acquired knowledge in Scrum skills and the development workflow.
-            <br />
-            {"\u00A0".repeat(4)}Throughout the boot-camp, even though I
-            encountered numerous challenges, I still enjoyed about that because
-            each time I resolved coding issues, I witnessed personal growth. The
-            sense of achievement I experienced further fueled my passion.
-            Although the boot-camp ended, my self-learning dosen't.✊
+            {"\u00A0".repeat(4)}A recent bootcamp graduate with self-motivation
+            and good self-learning skills. Eager to explore any opportunities to
+            enter the web development industry and contribute and enrich my
+            skill sets in future projects. Although the boot-camp ended, my
+            self-learning dosen't.✊
           </p>
         </div>
       </>
@@ -269,26 +255,6 @@ function App() {
   }
 
   function Projects() {
-    // const [burgerPic, setBurgerPic] = useState(burger1);
-    // const [extensionPic, setextensionPic] = useState(ex1);
-
-    // let burgerImage = require.context("./images/", false, /Burger.*\.png$/);
-    // let burgerImages = burgerImage.keys().map((image) => burgerImage(image));
-    // let chromeImage = require.context("./images/", false, /CE.*\.png$/);
-    // let chromeImages = chromeImage.keys().map((image) => chromeImage(image));
-
-    // function handleBurgerPic(imageArr) {
-    //   for (let i = 0; i < imageArr.length; i++) {
-    //     setInterval(() => {
-    //       setBurgerPic(burgerImages[i]);
-    //     }, 500);
-    //   }
-    // }
-
-    // function handleBurgerPicOut() {
-    //   setBurgerPic(burger1);
-    // }
-
     return (
       <>
         <div className="empty" ref={projectRef}></div>
@@ -296,12 +262,35 @@ function App() {
           <p className="title projectTitle">Projects</p>
           <div className="projectContainer">
             <div className="projectLeft">
-              <img
-                className="projectPic burger"
-                src={burgerGif}
-                // onMouseEnter={() => handleBurgerPic(burgerImages)}
-                // onMouseLeave={handleBurgerPicOut}
-              />
+              <img className="projectPic tgBot" src={tgBotGif} />
+            </div>
+            <div className="projectRight">
+              <div className="projectName">Telegram Bot of a electric shop</div>
+              <div className="projectDescibe">
+                <li>Product searching</li>
+                <li>Branch searching</li>
+                <li>Exporting search result</li>
+                <li>Play game to win coupon</li>
+                <li>
+                  Skills : <span className="nodeSkill">NodeJS</span>{" "}
+                  <span className="nestSkill">NestJS</span>{" "}
+                  <span className="mongoSkill">Mongodb</span>{" "}
+                </li>
+              </div>
+              <div className="link">
+                <a
+                  className="linkBtn"
+                  href="https://github.com/Danwwwww/TgBot/tree/main"
+                  target="blank"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="projectContainer">
+            <div className="projectLeft">
+              <img className="projectPic burger" src={burgerGif} />
             </div>
             <div className="projectRight">
               <div className="projectName">Online Burger Order Website</div>
@@ -400,44 +389,6 @@ function App() {
                 <a
                   className="linkBtn"
                   href="https://github.com/Danwwwww/Project/tree/main/KMB"
-                  target="blank"
-                >
-                  GitHub
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="projectContainer">
-            <div className="projectLeft">
-              <img
-                className="projectPic mtr"
-                src="https://raw.githubusercontent.com/Danwwwww/Project/main/image/MTR2.png"
-              />
-            </div>
-            <div className="projectRight">
-              <div className="projectName">MTR Arrival Time Query Website</div>
-              <div className="projectDescibe">
-                <li>
-                  Users can search the arrival time of different stations by
-                  pressing the button of route
-                </li>
-                <li>
-                  Skills : <span className="htmlSkill">HTML</span>{" "}
-                  <span className="cssSkill">CSS</span>{" "}
-                  <span className="domSkill">DOM</span>
-                </li>
-              </div>
-              <div className="link">
-                <a
-                  className="linkBtn"
-                  href="https://danwwwww.github.io/Project/MTR/"
-                  target="blank"
-                >
-                  Live
-                </a>
-                <a
-                  className="linkBtn"
-                  href="https://github.com/Danwwwww/Project/tree/main/MTR"
                   target="blank"
                 >
                   GitHub
